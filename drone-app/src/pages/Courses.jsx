7aaf44187590f2b992fab18743b39a5ce7465888
hannavas2022 <vasylets@gmail.com>
2025-01-22 15:20:10 -0600

    courses info changes
    Changed courses info and adjusted css for courses

--- drone-app/src/pages/Courses.jsx
+++ drone-app/src/pages/Courses.jsx
@@ -11,20 +11,20 @@ export default function Courses() {
                 {/* Course 1 */}
                 <div className="course-column">
                     <img src={logo} alt="logo" className="course-logo" />
-                    <h2 className="text-xl font-bold tracking-wider">Introduction to FPV Drone</h2>
-                    <div className="mt-4 text-md">
-                        <p>This exciting FPV Drone course is perfect for kids who are eager to experience the thrill of flight and explore the world from above!</p>
+                    <h2 className="text-center text-2xl font-bold tracking-wider">FPV Drone Pilot Level 1 (Youth)</h2>
+                    <div className="text-md mt-4">
+                        <p>This exciting FPV Drone course is perfect for youth who are eager to experience the thrill of flight and explore the world from above!</p>
                         <p className="mt-4"><strong>Included in this package:</strong></p>
-                        <ul className="pl-6 list-disc">
+                        <ul className="list-disc pl-6">
                             <li>Theoretical part</li>
-                            <li>Basic skill by flight simulator technology</li>
-                            <li>Basic practical flights</li>
+                            <li>Basic skills in drone flight simulator</li>
+                            <li>Basic practical skills on drones indoor</li>
                             <li>Safety protocols</li>
-                            <li>All equipment is provided!</li>
+                            <li>All equipment is provided: goggles, controller, drone, laptop, simulator</li>
                         </ul>
                         <p></p>
-                        <p className="mt-4"><strong>LEVEL: </strong>Absolutly begginer</p>
-                        <p className="mt-4"><strong>Duration: </strong>6 lessons</p>
+                        <p className="mt-4"><strong>LEVEL: </strong>Beginner</p>
+                        <p className="mt-4"><strong>Duration: </strong>6 lessons (9 hours in total)</p>
                         <div className="register-button">
                             <a href="https://retlife.ca/product/new-introduction-to-fpv-first-person-view-drone-technology-level-1-youth-ages-12-16-mondays-and-thursdays-february-10-march-3-2025-no-class-on-february-17-ecole-sun-valley-school/" target="_blank" rel="noopener noreferrer">
                                 <button>Register Now</button>
@@ -36,23 +36,23 @@ export default function Courses() {
                 {/* Course 2 */}
                 <div className="course-column">
                     <img src={logo} alt="logo" className="course-logo" />
-                    <h2 className="text-2xl font-bold tracking-wider">Introduction to FPV Drone</h2>
-                    <div className="mt-4 text-md">
+                    <h2 className="text-center text-2xl font-bold tracking-wider">FPV Drone Pilot Level 1 (Adults)</h2>
+                    <div className="text-md mt-4">
                         <p>This exciting FPV Drone course is perfect for adults who are eager to experience the thrill of flight and explore the world from above!</p>
                         <p className="mt-4"><strong>Included in this package:</strong></p>
-                        <ul className="pl-6 list-disc">
+                        <ul className="list-disc pl-6">
                             <li>Theoretical part</li>
-                            <li>Basic skill by flight simulator technology</li>
-                            <li>Basic practical flights</li>
+                            <li>Basic skills in drone flight simulator</li>
+                            <li>Basic practical skills on drones indoor</li>
                             <li>Safety protocols</li>
-                            <li>All equipment is provided!</li>
+                            <li>All equipment is provided: goggles, controller, drone, laptop, simulator</li>
                         </ul>
                         <p></p>
-                        <p className="mt-4"><strong>LEVEL: </strong>Absolutly begginer</p>
-                        <p className="mt-4"><strong>Duration: </strong>6 lessons</p>
+                        <p className="mt-4"><strong>LEVEL: </strong>Beginner</p>
+                        <p className="mt-4"><strong>Duration: </strong>6 lessons (9 hours in total)</p>
                         <div className="coming-button">
                             <a href="/" target="_blank" rel="noopener noreferrer">
-                                <button disabled>Coming soon</button>
+                                <button disabled>Spring 2025</button>
                             </a>
                         </div>
                     </div>
@@ -61,20 +61,21 @@ export default function Courses() {
                 {/* Course 3 */}
                 <div className="course-column">
                     <img src={logo} alt="logo" className="course-logo" />
-                    <h2 className="text-2xl font-bold tracking-wider">Advanced FPV Drone Skills</h2>
-                    <div className="mt-4 text-md">
+                    <h2 className="text-center text-2xl font-bold tracking-wider">Advanced FPV Drone Skills</h2>
+                    <div className="text-md mt-4">
                         <p>This course builds on the basics to help you master advanced flight techniques and explore more exciting drone maneuvers</p>
                         <p className="mt-4"><strong>Included in this package:</strong></p>
-                        <ul className="pl-6 list-disc">
+                        <ul className="list-disc pl-6">
                             <li>Advanced flight techniques</li>
                             <li>Flight path planning and execution</li>
                             <li>Handling complex drone maneuvers</li>
                             <li>Advanced safety protocols</li>
-                            <li>All equipment is provided!</li>
+                            <li>All equipment is provided: goggles, controller, drone, laptop, simulator</li>
                         </ul>
                         <p></p>
                         <p className="mt-4"><strong>LEVEL: </strong>Beginner with basic experience</p>
                         <p className="mt-4"><strong>Duration: </strong>6 lessons</p>
+                        <p className="mt-10"></p>
                         <div className="coming-button">
                             <a href="/" target="_blank" rel="noopener noreferrer">
                                 <button disabled>Coming soon</button>
@@ -86,20 +87,21 @@ export default function Courses() {
                 {/* Course 4 */}
                 <div className="course-column">
                     <img src={logo} alt="logo" className="course-logo" />
-                    <h2 className="text-2xl font-bold tracking-wider">Drone Mastery</h2>
-                    <div className="mt-4 text-md">
+                    <h2 className="text-center text-2xl font-bold tracking-wider">Drone Mastery</h2>
+                    <div className="text-md mt-4">
                         <p>This course is designed for those ready to master complex maneuvers and refine their flying skills with precision and confidence.</p>
                         <p className="mt-4"><strong>Included in this package:</strong></p>
-                        <ul className="pl-6 list-disc">
+                        <ul className="list-disc pl-6">
                             <li>Mastering high-speed flights</li>
                             <li>Exploring acrobatic drone maneuvers</li>
                             <li>Flight under challenging conditions</li>
                             <li>Drone racing basics</li>
-                            <li>All equipment is provided!</li>
+                            <li>All equipment is provided: goggles, controller, drone, laptop, simulator</li>
                         </ul>
                         <p></p>
                         <p className="mt-4"><strong>LEVEL: </strong>Intermediate with advanced skills</p>
                         <p className="mt-4"><strong>Duration: </strong>6 lessons</p>
+                        <p className="mt-10"></p>
                         <div className="coming-button">
                             <a href="/" target="_blank" rel="noopener noreferrer">
                                 <button disabled>Coming soon</button>
